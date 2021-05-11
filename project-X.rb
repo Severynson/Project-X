3b25bda2208c1b64bc6ca6e2eb5bd4d2c2b04604
severyn <severynson.kurach@gmail.com>
2021-05-11 18:17:20 +0300

    from_the_begining_till_1_part_of_test

--- project-X.rb
+++ project-X.rb
@@ -1,65 +1,77 @@
 ### This part of code had to show the main menu for user ###
 puts "Hello, choose please the them that you are interested to learn:\n1- Git;\n2- Computer;\n3- Main terms;\n4- Create your own list."
+puts ""
+
 class WordLists
   attr_accessor :git, :computer, :main_terms, :your_own_list
+
   def choosing(git = 0, computer = 0, main_terms = 0, your_own_list = 0)
     #git = #в цій змінній повинен зберігатись рідер конкретного текстового документу.
-  input = gets.chomp
+    print "Your choice:"
+    input = gets.chomp
+    puts ""
+    puts "---List of words that you will meet in tests---"
 
-  if input == "1"
+    if input == "1"
 
-    lines = []
-    File.open("Git.txt") do |review_file|
-      lines = review_file.readlines
-      puts lines
+      lines = []
+      File.open("Git.txt") do |review_file|
+        lines = review_file.readlines
+        puts lines
       end
 
+      #####
 
-    #####
-
-      elsif input == "2"
+    elsif input == "2"
 
       lines = []
       File.open("Computer.txt") do |review_file|
         lines = review_file.readlines
         puts lines
-        end
-
+      end
 
       #####
 
-        elsif input == "3"
+    elsif input == "3"
 
-        lines = []
-        File.open("Computer.txt") do |review_file|
-          lines = review_file.readlines
-          puts lines
+      lines = []
+      File.open("Computer.txt") do |review_file|
+        lines = review_file.readlines
+        puts lines
 
-        end
+      end
 
-
-    # list = git
-    # p list
-  end
+      # list = git
+      # p list
+    end
 
     puts " "
     puts "Do you want to continue making tests or still need to learn this words better?(yes/no)"
     variable = gets.chomp
     if variable == "yes"
-        puts "this part of program is still in the process of develop."
-        element_check = lines.find_all do |el|
-          write = gets.chomp
+      puts "Here is a word and it's translation, enter please the English version five times."
+      element_check = lines.find_all do |el|
+        puts "***"
+        p el.chomp
+        write = gets.upcase.chomp
 
-          #p el.split(" ").first
-          if write == "#{el.split(" ").first}, #{el.split(" ").first}, #{el.split(" ").first}, #{el.split(" ").first}, #{el.split(" ").first}"
-            puts true
-          else
-            puts false
+
+        while write != "#{el.upcase.split(" ").first}, #{el.upcase.split(" ").first}, #{el.upcase.split(" ").first}, #{el.upcase.split(" ").first}, #{el.upcase.split(" ").first}"
+          puts " "
+          puts "Your made some mistake, enter please correct translation of word."
+          write = gets.upcase.chomp
           end
-        end
-      else
-        return 0
+    #     if write == "#{el.split(" ").first}, #{el.split(" ").first}, #{el.split(" ").first}, #{el.split(" ").first}, #{el.split(" ").first}"
+    #       puts true
+    #     else
+    #       puts false
+    #     end
       end
+    # else
+    #   return 0
+    end
+
+    puts "Now you can start second part of tests:"
 
     ###########   Second part of learning #############
     #
@@ -78,18 +90,5 @@ class WordLists
 
     ###########################
 
-    #odd_numbers_sum = numbers.find_all(&:odd?).sum
 
-
-
-  end
-
-  end
-
-
-
-
-
-salo = WordLists.new
-salo.choosing
 
