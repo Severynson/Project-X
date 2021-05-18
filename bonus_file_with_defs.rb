a079707f9a1e03afe41a3b815ee99f0cccaebba4
severyn <severynson.kurach@gmail.com>
2021-05-18 21:53:35 +0300

    refactored

--- bonus_file_with_defs.rb
+++ bonus_file_with_defs.rb
@@ -2,72 +2,33 @@
 ###########################    General methods    ###############################
 #################################################################################
 require 'fileutils'
-########    This method helps to add a new (your own) wordlist    #######
-def own_wordlist
-  puts "Write the name of your new list:"
-  name_of_file = gets.chomp.to_s.<<".txt"
-  puts name_of_file
-  file = File.open(name_of_file, "w+")
-  eng_error_text = "Your word isn't English, please write English word"
-  rus_error_text = "Your word isn't Russian, please write Russian word"
-  loop do
 
-    puts "Write STOP if you wanna stop writing and save it\nWrite English word:"
-    en_word = gets
-    eng_word = en_word.chomp.to_s.<<" - "
-    eng_correction = en_word.ascii_only?
-    if eng_correction == false
-      puts eng_error_text
-    end
 
-    eng_correction1 = en_word.is_a?(Integer)
-    if eng_correction1 == true
-      puts eng_error_text
-    end
-    while eng_correction == false && eng_correction1 == true
-      puts "Write English word:"
-      en_word = gets
-    end
-    file.write(eng_word)
-
-    if en_word == "STOP - "
-      file.close
-      break
-    end
-
-    puts "Write STOP if you wanna stop writing and save it\nWrite Russian word:"
-    ru_word = gets
-    rus_word = ru_word.chomp.to_s.<<"\n"
-
-    rus_correction = ru_word.ascii_only?
-    if rus_correction == true
-      puts rus_error_text
-    end
-
-    rus_correction1 = ru_word.is_a?(Integer)
-    if rus_correction1 == true
-      puts rus_error_text
-    end
-    file.write(rus_word)
-    while rus_correction == true && rus_correction1 == true
-      puts "Write Russian word:"
-      ru_word = gets
-    end
-    if rus_word == "STOP\n"
-      #rus_word.freeze
-      file.close()
-      break
-    end
-  end
-  ###
-  removing_stopstop = File.open('output.txt', 'w') do |out_file|
-    File.foreach(name_of_file) do |line|
-      out_file.puts line unless line =~ /STOP/
-    end
-  end
-  FileUtils.mv('output.txt', name_of_file)
-  ###
+def puts_something
+  puts ""
+  puts "---List of words that you will meet in tests---"
 end
+
+def read_this_file (file_to_read)
+  @array_with_strings_from_txt = []
+  File.open(file_to_read) do |review_file|
+    @array_with_strings_from_txt = review_file.readlines
+    puts @array_with_strings_from_txt
+  end
+end
+
+def start_test_1_or_not
+  puts " "
+  puts "Do you want to continue making tests or still need to learn this words better?(yes/no)"
+  variable = gets.chomp
+  if variable.upcase == "YES".chomp
+    puts ""
+    task_1
+  else
+    return 0
+  end
+end
+
 
 #################################################################################
 ########################    The main menu's method    ###########################
@@ -116,36 +77,77 @@ def main_menu(input)
       raise "Incorrect input!"
     rescue StandardError => input6
       my_gets = gets.chomp
-      input_for_choosing_them(my_gets)
+      main_menu(my_gets)
     end
   end
 end
-    #######
 
-def puts_something
-  puts ""
-  puts "---List of words that you will meet in tests---"
-end
 
-def read_this_file (zminnna)
-  @array_with_strings_from_txt = []
-  File.open(zminnna) do |review_file|
-    @array_with_strings_from_txt = review_file.readlines
-    puts @array_with_strings_from_txt
+############    This method helps to add a new (your own) wordlist    ###########
+def own_wordlist
+  puts "Write the name of your new list:"
+  name_of_file = gets.chomp.to_s.<< ".txt"
+  puts name_of_file
+  file = File.open(name_of_file, "w+")
+  eng_error_text = "Your word isn't English, please write English word"
+  rus_error_text = "Your word isn't Russian, please write Russian word"
+  loop do
+
+    puts "Write STOP if you wanna stop writing and save it\nWrite English word:"
+    en_word = gets
+    eng_word = en_word.chomp.to_s.<< " - "
+    eng_correction = en_word.ascii_only?
+    if eng_correction == false
+      puts eng_error_text
+    end
+
+    eng_correction1 = en_word.is_a?(Integer)
+    if eng_correction1 == true
+      puts eng_error_text
+    end
+    while eng_correction == false && eng_correction1 == true
+      puts "Write English word:"
+      en_word = gets
+    end
+    file.write(eng_word)
+
+    if eng_word == "STOP - "
+      file.close
+      break
+    end
+
+    puts "Write STOP if you wanna stop writing and save it\nWrite Russian word:"
+    ru_word = gets
+    rus_word = ru_word.chomp.to_s.<< "\n"
+
+    rus_correction = ru_word.ascii_only?
+    if rus_correction == true
+      puts rus_error_text
+    end
+
+    rus_correction1 = ru_word.is_a?(Integer)
+    if rus_correction1 == true
+      puts rus_error_text
+    end
+    file.write(rus_word)
+    while rus_correction == true && rus_correction1 == true
+      puts "Write Russian word:"
+      ru_word = gets
+    end
+    if rus_word == "STOP\n"
+      #rus_word.freeze
+      file.close()
+      break
+    end
   end
-end
-
-
-def start_test_1_or_not
-  puts " "
-  puts "Do you want to continue making tests or still need to learn this words better?(yes/no)"
-  variable = gets.chomp
-  if variable.upcase == "YES".chomp
-    puts ""
-    task_1
-  else
-    return 0
+  ###
+  removing_stopstop = File.open('output.txt', 'w') do |out_file|
+    File.foreach(name_of_file) do |line|
+      out_file.puts line unless line =~ /STOP/
+    end
   end
+  FileUtils.mv('output.txt', name_of_file)
+  ###
 end
 
 
@@ -154,8 +156,7 @@ end
 #################################################################################
 
 
-#####################   This is s method for a first exercise   #################
-
+#####################   This is s method for a first exercise   ##################
 
 def task_1
   puts "Here is a word and it's translation, enter please the English version five times."
@@ -169,55 +170,52 @@ def task_1
     #   p variable_of_input
     # is_it_include_variable_of_input =
     #   write.include?(variable_of_input)
-      write = gets.upcase.chomp
+    write = gets.upcase.chomp
     while correct_word.include?(write) == false
-        puts " "
-        puts "Your made some mistake, enter please correct translation of word."
-        write = gets.upcase.chomp
+      puts " "
+      puts "Your made some mistake, enter please correct translation of word."
+      write = gets.upcase.chomp
     end
   end
 end
 
-
-
-#####################   That are methods for a second task   ###################
+#####################   That are methods for a second exercise   ###################
 
 def exercise_2
   read_this_file (@variable_for_remembering_them)
   @array_with_strings_from_txt.each do |el|
-  user_input = Thread.new do
-    p el.upcase.split(" - ").last.chomp
-    @write = gets.upcase.chomp
-    Thread.current[:value] = gets.chomp
-  end
-
-  Thread.new { sleep 6; user_input.kill; puts }
-
-  user_input.join
-  if user_input[:value]
-    puts "User entered #{user_input[:value]}"
-  else
-    puts "Timer expired"
-
-    if @write != "#{el.upcase.split(" ").first}"
-      puts " "
-      puts "Your made some mistake, enter please correct translation of word."
-    elsif @write == "#{el.upcase.split(" ").first}"
-      puts "You are winner!"
+    user_input = Thread.new do
+      p el.upcase.split(" - ").last.chomp
+      @write = gets.upcase.chomp
+      Thread.current[:value] = gets.chomp
     end
 
+    Thread.new { sleep 6; user_input.kill; puts }
+
+    user_input.join
+    if user_input[:value]
+      puts "User entered #{user_input[:value]}"
+    else
+      puts "Timer expired"
+
+      if @write != "#{el.upcase.split(" ").first}"
+        puts " "
+        puts "Your made some mistake, enter please correct translation of word."
+      elsif @write == "#{el.upcase.split(" ").first}"
+        puts "You are winner!"
+      end
+
+    end
   end
 end
-end
 
-#####################   That are methods for a third task   ###################
-
+#####################   That are methods for a third exercise   ###################
 
 def mistake_first_in_exercise_3
   first_letters = @el_incorrect.downcase.split("")
   print "You write something incorrectly.\nChange this situation please.\nHere is the first letter of your word:\""
   print first_letters[0].to_s
-  puts"\"."
+  puts "\"."
   ###
   @write = gets.upcase.chomp
   if @write == @el_incorrect.upcase.split(" - ").first.chomp
@@ -226,12 +224,13 @@ def mistake_first_in_exercise_3
     mistake_second_in_exercise_3
   end
 end
+
 ###
 def mistake_second_in_exercise_3
   first_letters = @el_incorrect.downcase.split("")
   print "You write something incorrectly.\nChange this situation please.\nHere are the first three letters of your word:\""
   print first_letters[0..2].join
-  puts"\"."
+  puts "\"."
   ###
   @write = gets.upcase.chomp
   if @write == @el_incorrect.upcase.split(" - ").first.chomp
@@ -240,6 +239,7 @@ def mistake_second_in_exercise_3
     mistake_third_in_exercise_3
   end
 end
+
 ###
 
 def mistake_third_in_exercise_3
@@ -247,7 +247,7 @@ def mistake_third_in_exercise_3
   first_letter = @el_incorrect.downcase.split("")
   print "You write something incorrectly.\nChange this situation please.\nHere is the half of word that you need to write:\""
   print first_letter[0..half].join
-  puts"\"."
+  puts "\"."
   ###
   @write = gets.upcase.chomp
   if @write == @el_incorrect.upcase.split(" - ").first.chomp
@@ -263,7 +263,6 @@ def mistake_third_in_exercise_3
 end
 
 ############################
-
 
 def exercise_3
   read_this_file (@variable_for_remembering_them)
