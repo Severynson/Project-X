3b1954e12bbb17badabbb95824b97209114b5db9
Radchenko Valeria <valeriaradcenko95@gmail.com>
2021-05-20 18:24:17 +0200

    Here I changed the names of my variables.I rename variable 'ru_word' on 'users_input_rus', and variable 'en_word' on 'users_input_eng'. I hope it will be clearly and more readble

--- project-X.rb
+++ project-X.rb
@@ -1,30 +1,32 @@
 require 'fileutils'
 
-###############Create The New List Part##############################################
+#Create The New List Part#
 puts "Write the name of your new list:"
 name_of_file = gets.chomp.to_s.<<".txt"
 puts name_of_file
 file = File.open(name_of_file, "w+")
 eng_error_text = "Your word isn't English, please write English word"
 rus_error_text = "Your word isn't Russian, please write Russian word"
-################################English Word Part#########################################
+
+#English Word Part#
+
 loop do
   puts "Write STOP if you wanna stop writing and save it\nWrite English word:"
-  en_word = gets
-  eng_word = en_word.chomp.to_s << " - "
+  users_input_eng = gets
+  # eng_word = users_input_eng.chomp.to_s << " - "
 
-    eng_correction1 = en_word.match?(/[qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]/i)
+    eng_correction1 = users_input_eng.match?(/[qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]/i)
 
   while eng_correction1 == false
-    eng_correction1 = en_word.match?(/[qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]/i)
+    eng_correction1 = users_input_eng.match?(/[qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]/i)
     if eng_correction1 == false
       puts eng_error_text
-      en_word = gets
+      users_input_eng = gets
     else
       break
     end
   end
-  eng_word = en_word.chomp.to_s << " - "
+  eng_word = users_input_eng.chomp.to_s << " - "
   file.write(eng_word)
 
   if eng_word == "STOP - "
@@ -32,31 +34,31 @@ loop do
     break
   end
 
-  #############################################Russian Word Part#######################################
+  #Russian Word Part#
 
   puts "Write STOP if you wanna stop writing and save it\nWrite Russian word:"
-  ru_word = gets
-  rus_word = ru_word.chomp.to_s.<<"\n"
+  users_input_rus = gets
+  # rus_word = ru_word.chomp.to_s.<<"\n"
 
-  rus_correction1 = ru_word.match?(/[йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]/i)
+  rus_correction1 = users_input_rus.match?(/[йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]/i)
 
   while rus_correction1 == false
-    rus_correction1 = ru_word.match?(/[йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]/i)
+    rus_correction1 = users_input_rus.match?(/[йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]/i)
     if rus_correction1 == false
       puts rus_error_text
-      ru_word = gets
+      users_input_rus = gets
     else
       break
     end
   end
-  rus_word = ru_word.chomp.to_s.<<"\n"
+  rus_word = users_input_rus.chomp.to_s.<< "\n"
   file.write(rus_word)
   if rus_word == "STOP\n"
     file.close()
     break
   end
 end
-removing_stopstop = File.open('output.txt', 'w') do |out_file|
+  File.open('output.txt', 'w') do |out_file|
   File.foreach(name_of_file) do |line|
     out_file.puts line unless line =~ /STOP/
   end
