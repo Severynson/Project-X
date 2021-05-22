7bdeba19667d96220686a37844bdfae87f459d73
Radchenko Valeria <valeriaradcenko95@gmail.com>
2021-05-22 23:11:14 +0200

    Here's my 5th version of this part of programm. Unfortunatelly, still have too much different repeats and this code is unclean. I have to make more usefull structure for this code. So, I need some help

--- project-X.rb
+++ project-X.rb
@@ -1,25 +1,25 @@
-require 'fileutils'
+require 'fileutils'  # This library helps to remove STOP from the end of the list
 
 #Create The New List Part#
-puts "Write the name of your new list:"
-name_of_file = gets.chomp.to_s.<<".txt"
-puts name_of_file
+puts "Write the name of your new list:" #
+name_of_file = gets.chomp.to_s.<< ".txt"
+puts "#{name_of_file} list was succesfully created!\n Remember! If you want to stop writing words, just write 'STOP'."
 file = File.open(name_of_file, "w+")
 eng_error_text = "Your word isn't English, please write English word"
 rus_error_text = "Your word isn't Russian, please write Russian word"
 
 #English Word Part#
 
-loop do
-  puts "Write STOP if you wanna stop writing and save it\nWrite English word:"
+loop do # This huge loop helps repeat asking user to put English and Russian word again and again till user puts STOP
+  puts "Write English word:"
   users_input_eng = gets
-  # eng_word = users_input_eng.chomp.to_s << " - "
 
-    eng_correction1 = users_input_eng.match?(/[qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]/i)
+  eng_correction = users_input_eng.match?(/[qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]/i) # Checking, if users input is correct.
+  # If user write number, symbol or Russian word, program will ask to write English word again and puts the message with error
 
-  while eng_correction1 == false
-    eng_correction1 = users_input_eng.match?(/[qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]/i)
-    if eng_correction1 == false
+  while eng_correction == false # This loop repeat asking Eng word if input is uncorrect
+    eng_correction = users_input_eng.match?(/[qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]/i) # Checks again
+    if eng_correction == false
       puts eng_error_text
       users_input_eng = gets
     else
@@ -35,16 +35,15 @@ loop do
   end
 
   #Russian Word Part#
-
-  puts "Write STOP if you wanna stop writing and save it\nWrite Russian word:"
+  # Russian part works the same as the English part, but the names of variables have some changes
+  puts "Write Russian word:"
   users_input_rus = gets
-  # rus_word = ru_word.chomp.to_s.<<"\n"
 
-  rus_correction1 = users_input_rus.match?(/[йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]/i)
+  rus_correction = users_input_rus.match?(/[йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]/i)
 
-  while rus_correction1 == false
-    rus_correction1 = users_input_rus.match?(/[йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]/i)
-    if rus_correction1 == false
+  while rus_correction == false
+    rus_correction = users_input_rus.match?(/[йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]/i)
+    if rus_correction == false
       puts rus_error_text
       users_input_rus = gets
     else
@@ -58,7 +57,7 @@ loop do
     break
   end
 end
-  File.open('output.txt', 'w') do |out_file|
+File.open('output.txt', 'w') do |out_file| # Helps to remove STOP
   File.foreach(name_of_file) do |line|
     out_file.puts line unless line =~ /STOP/
   end
